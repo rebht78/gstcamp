dd517d143d016d64173a1312af372e10434fe932
Sohel Shaikh <rebht78@gmail.com>
2019-08-27 22:03:24 +0530

    Added LoginBean, LoginService interface

---
+++ gstcamp/src/java/com/gstcamp/service/LoginService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gstcamp.service;
+
+import com.gstcamp.bean.LoginBean;
+import java.util.List;
+
+/**
+ *
+ * @author aziz
+ */
+public interface LoginService {
+    List checkLogin(LoginBean loginBean);
+}
